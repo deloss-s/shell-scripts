5ff9b969a885351e9daec9f3131d48e3f32664d1
deloss-s <kamil.saypulaev@gmail.com>
2026-04-10 05:37:17 +0300

    new paths to bckp config files

--- backup.sh
+++ backup.sh
@@ -43,20 +43,20 @@ log "=== BACKUP START ==="
 
 # ========= MIGRATIONS =========
 
-run_step "Local root .config sync" /usr/sbin/rsync -a --delete /root/.config/ /root/linux-all/root/.config/
-run_step "Local fstab sync" /usr/sbin/rsync -a --delete /etc/fstab /root/linux-all/etc/fstab
-run_step "Local motd sync" /usr/sbin/rsync -a --delete /etc/motd /root/linux-all/etc/motd
-run_step "Local dnf sync" /usr/sbin/rsync -a --delete /etc/dnf/ /root/linux-all/etc/dnf/
-run_step "Local nginx sync" /usr/sbin/rsync -a --delete /etc/nginx/ /root/linux-all/etc/nginx/
-run_step "Local default sync" /usr/sbin/rsync -a --delete /etc/default/ /root/linux-all/etc/default/
-run_step "Local letsencrypt sync" /usr/sbin/rsync -a --delete /etc/letsencrypt/ /root/linux-all/etc/letsencrypt/
-#run_step "Local samba sync" /usr/sbin/rsync -a --delete /etc/samba/ /root/linux-all/etc/samba/
-#run_step "Local transmission-daemon-etc sync" /usr/sbin/rsync -a --delete /etc/transmission-daemon/ /root/linux-all/etc/transmission-daemon/
+run_step "Local root .config sync" /usr/sbin/rsync -a --delete /root/.config/ /root/linux-all/git/config-files/root.config/
+run_step "Local fstab sync" /usr/sbin/rsync -a --delete /etc/fstab /root/linux-all/git/config-files/etc/fstab
+run_step "Local motd sync" /usr/sbin/rsync -a --delete /etc/motd /root/linux-all/git/config-files/etc/motd
+run_step "Local dnf sync" /usr/sbin/rsync -a --delete /etc/dnf/ /root/linux-all/git/config-files/etc/dnf/
+run_step "Local nginx sync" /usr/sbin/rsync -a --delete /etc/nginx/ /root/linux-all/git/config-files/etc/nginx/
+run_step "Local default sync" /usr/sbin/rsync -a --delete /etc/default/ /root/linux-all/git/config-files/etc/default/
+run_step "Local letsencrypt sync" /usr/sbin/rsync -a --delete /etc/letsencrypt/ /root/linux-all/git/config-files/etc/letsencrypt/
+#run_step "Local samba sync" /usr/sbin/rsync -a --delete /etc/samba/ /root/linux-all/git/config-files/etc/samba/
+#run_step "Local transmission-daemon-etc sync" /usr/sbin/rsync -a --delete /etc/transmission-daemon/ /root/linux-all/git/config-files/etc/transmission-daemon/
 #run_step "Local transmission-daemon-var sync" /usr/sbin/rsync -a --delete /var/lib/transmission/.config/transmission-daemon/ /root/linux-all/var/lib/transmission/.config/transmission-daemon/
 #run_step "Local docker-var sync" /usr/sbin/rsync -a --delete /var/lib/docker/ /root/linux-all/var/lib/docker/
-#run_step "Local wireguard sync" /usr/sbin/rsync -a --delete /etc/wireguard/ /root/linux-all/etc/wireguard/
-run_step "Local systemd sync" /usr/sbin/rsync -a --delete /etc/systemd/ /root/linux-all/etc/systemd/
-run_step "Local resolv.conf sync" /usr/sbin/rsync -a --delete /etc/resolv.conf /root/linux-all/etc/resolv.conf
+#run_step "Local wireguard sync" /usr/sbin/rsync -a --delete /etc/wireguard/ /root/linux-all/git/config-files/etc/wireguard/
+run_step "Local systemd sync" /usr/sbin/rsync -a --delete /etc/systemd/ /root/linux-all/git/config-files/etc/systemd/
+run_step "Local resolv.conf sync" /usr/sbin/rsync -a --delete /etc/resolv.conf /root/linux-all/git/config-files/etc/resolv.conf
 run_step "Local linux-all copy" /usr/sbin/rsync -a --delete /root/linux-all/ /root/linux-all-copy/
 #run_step "Local linux-all mergerfs-7tb copy" /usr/sbin/rsync -a --delete /root/linux-all/ /mnt/mergerfs-7tb/backups/linux-all/
 run_step "Local linux-all sync" /usr/sbin/rsync -a --delete /root/linux-all/ /mnt/nas/nas/01-Main/01-Important/01-backups/os/linux-all/
